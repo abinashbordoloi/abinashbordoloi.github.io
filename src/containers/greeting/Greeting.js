ff36878477226e486f9ba2fe217a665302918cb7
Abinash Bordoloi <75088946+Abinash-bordoloi07@users.noreply.github.com>
2023-06-14 05:01:16 +0530

    ch

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -40,12 +40,8 @@ export default function Greeting(props) {
               </div>
             </div>
           </div>
-          <div className="greeting-image-div">
-            {/* <img
-							alt="saad sitting on table"
-							src={require("../../assests/images/feelingProud.svg")}
-						></img> */}
-            <FeelingProud theme={theme} />
+          <div className="image-container">
+            <img src="src/assets/images/profile.jpg" alt="Your Image"></img>
           </div>
         </div>
       </div>
