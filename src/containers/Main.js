50cebc2870f13c3d58b17a4b1c80eb8aa40364b0
Abinash Bordoloi <75088946+Abinash-bordoloi07@users.noreply.github.com>
2023-06-23 23:21:22 +0530

    splash screen and main.js for route changed

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
-import Splash from "../pages/splash/Splash";
+import Splashscreen from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
@@ -9,126 +9,69 @@ import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
-import LoaderLogo from "../components/Loader/LoaderLogo";
+// import LoaderLogo from "../components/Loader/LoaderLogo";
 
 export default class Main extends Component {
   render() {
-    if (settings.isSplash) {
-      return (
-        <div>
-          <HashRouter basename="/">
-            <Switch>
-              <Route path="/" />
+    const { isSplash } = settings;
 
-              <Route
-                path="/home"
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/experience"
-                exact
-                render={(props) => (
-                  <Experience {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/education"
-                render={(props) => (
-                  <Education {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/splash"
-                render={(props) => (
-                  <Splash {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="*"
-                render={(props) => (
-                  <Error404 {...props} theme={this.props.theme} />
-                )}
-              />
-            </Switch>
-          </HashRouter>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <HashRouter basename="/">
-            <Switch>
-              <Route
-                path="/"
-                exact
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/home"
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/experience"
-                exact
-                render={(props) => (
-                  <Experience {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/education"
-                render={(props) => (
-                  <Education {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              {/* <Route
-							path="/splash"
-							render={(props) => (
-								<Splash
-									{...props}
-									theme={this.props.theme}
-								/>
-							)}
-						/> */}
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
-                )}
-              />
-            </Switch>
-          </HashRouter>
-        </div>
-      );
-    }
+    return (
+      <div>
+        {isSplash && <Splashscreen showSplash={true} />}
+        <HashRouter basename="/">
+          <Switch>
+            <Route exact path="/" Splashscreen />
+
+            <Route
+              path="/home"
+              render={(props) => <Home {...props} theme={this.props.theme} />}
+            />
+            <Route
+              path="/experience"
+              exact
+              render={(props) => (
+                <Experience {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/education"
+              render={(props) => (
+                <Education {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/opensource"
+              render={(props) => (
+                <Opensource {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/contact"
+              render={(props) => (
+                <Contact {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/splash"
+              render={(props) => (
+                <Splashscreen {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/projects"
+              render={(props) => (
+                <Projects {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="*"
+              render={(props) => (
+                <Error404 {...props} theme={this.props.theme} />
+              )}
+            />
+          </Switch>
+        </HashRouter>
+      </div>
+    );
   }
 }
--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -1,60 +1,137 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import { Component } from "react";
 
-const Splashscreen = ({ showSplash }) => {
-  const [videoLoaded, setVideoLoaded] = useState(false);
+// const Splashscreen  = ({ showSplash }) => {
+//   const [videoLoaded, setVideoLoaded] = useState(false);
 
-  useEffect(() => {
-    const videoElement = document.getElementById("splash-video");
-    videoElement.addEventListener("loadeddata", () => {
-      setVideoLoaded(true);
-      setTimeout(() => {
-        // Hide the splash screen after the desired duration
-        document.getElementById("splash-screen").style.display = "none";
-      }, 3000); // Adjust the duration in milliseconds (e.g., 3000 for 3 seconds)
+//   useEffect(() => {
+//     const videoElement = document.getElementById("splash-video");
+//     videoElement.addEventListener("loadeddata", () => {
+//       setVideoLoaded(true);
+//       setTimeout(() => {
+//         // Hide the splash screen after the desired duration
+//         document.getElementById("splash-screen").style.display = "none";
+//       }, 3000); // Adjust the duration in milliseconds (e.g., 3000 for 3 seconds)
 
-      // Cleanup event listener on component unmount
-      return () => {
-        videoElement.removeEventListener("loadeddata", () => {
-          setVideoLoaded(true);
-        });
-      };
-    });
-  }, []);
+//       // Cleanup event listener on component unmount
+//       return () => {
+//         videoElement.removeEventListener("loadeddata", () => {
+//           setVideoLoaded(true);
+//         });
+//       };
+//     });
+//   }, []);
 
-  const splashScreenStyles = {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f1f1f1",
-  };
+//   const splashScreenStyles = {
+//     position: "fixed",
+//     top: "0",
+//     left: "0",
+//     width: "100%",
+//     height: "100%",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#f1f1f1",
+//   };
 
-  const loadingStyles = {
-    fontSize: "24px",
-    color: "#333",
-  };
+//   const loadingStyles = {
+//     fontSize: "24px",
+//     color: "#333",
+//   };
 
-  if (!showSplash) {
-    return null;
+//   if (!showSplash) {
+//     return null;
+//   }
+
+//   return (
+//     <div id="splash-screen" style={splashScreenStyles}>
+//       {!videoLoaded && (
+//         <div style={loadingStyles}>
+//           {/* Display a loading message or spinner */}
+//           <p>Loading...</p>
+//         </div>
+//       )}
+//       <video id="splash-video" autoPlay muted>
+//         <source src="../../assests/images/LogoSplash.mp4" type="video/mp4" />
+//       </video>
+//     </div>
+//   );
+// };
+
+// export default Splashscreen;
+
+import React, { Component } from "react";
+
+class Splashscreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoLoaded: false,
+    };
   }
 
-  return (
-    <div id="splash-screen" style={splashScreenStyles}>
-      {!videoLoaded && (
-        <div style={loadingStyles}>
-          {/* Display a loading message or spinner */}
-          <p>Loading...</p>
-        </div>
-      )}
-      <video id="splash-video" autoPlay muted>
-        <source src="../../assests/images/LogoSplash.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+  componentDidMount() {
+    const videoElement = document.getElementById("splash-video");
+    if (videoElement) {
+      videoElement.addEventListener("loadeddata", this.handleVideoLoaded);
+    }
+  }
+
+  componentWillUnmount() {
+    const videoElement = document.getElementById("splash-video");
+    videoElement.removeEventListener("loadeddata", this.handleVideoLoaded);
+  }
+
+  handleVideoLoaded = () => {
+    this.setState({ videoLoaded: true });
+    setTimeout(() => {
+      // Hide the splash screen after the desired duration
+      document.getElementById("splash-screen").style.display = "none";
+    }, 3000); // Adjust the duration in milliseconds (e.g., 3000 for 3 seconds)
+  };
+
+  render() {
+    const { showSplash } = this.props;
+    const { videoLoaded } = this.state;
+
+    const splashScreenStyles = {
+      position: "fixed",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#f1f1f1",
+    };
+
+    const loadingStyles = {
+      fontSize: "24px",
+      color: "#333",
+    };
+
+    if (!showSplash) {
+      return null;
+    }
+
+    return (
+      <div id="splash-screen" style={splashScreenStyles}>
+        {!videoLoaded && (
+          <div style={loadingStyles}>
+            {/* Display a loading message or spinner */}
+            <p>Loading...</p>
+          </div>
+        )}
+        <video id="splash-video" autoPlay muted>
+          <source
+            src={require("../../assests/images/LogoSplash.mp4")}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    );
+  }
+}
 
 export default Splashscreen;
